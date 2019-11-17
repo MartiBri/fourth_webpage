37cfc9515f14c7cb7c7068bc8c19ae72bcd56091
MartiBri <53783053+MartiBri@users.noreply.github.com>
2019-11-17 12:32:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function goStupid(){
+  let adjectivE = document.getElementById("onE").value;
+  let nouN = document.getElementById("twO").value;
+  let animaL = document.getElementById("threE").value;
+  let noisE = document.getElementById("fouR").value;
+
+  document.getElementById("outputAnswer").innerHTML = "<h3><span class='anSwer'>Brian's " + adjectivE + " car had a " + nouN + " that gave birth to a(n) " + animaL + " that made the sound " + noisE + "!</span>";
+
+
+}
